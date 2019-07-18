34fc8036b5df7ee02c56e890d19d8436064b0fcf
GUOED3 <EDDY.GUO@OOCL.COM>
2019-07-18 17:21:35 +0800

    [GRD] finish Controller test of Story1-AC1:add a parking lot

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -1,7 +1,10 @@
 package com.thoughtworks.parking_lot.controller;
 
+import com.thoughtworks.parking_lot.model.ParkingLot;
 import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,9 @@ public class ParkingLotController {
     @Autowired
     private ParkingLotRepository repository;
 
+    @PostMapping("/parkinglots")
+    public ParkingLot createParkingLot(@RequestBody ParkingLot parkingLot){
+        return repository.save(parkingLot);
+    }
 
 }
--- src/test/java/com/thoughtworks/parking_lot/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/ParkingLotControllerTest.java
@@ -1,11 +1,52 @@
 package com.thoughtworks.parking_lot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import com.thoughtworks.parking_lot.controller.ParkingLotController;
+import com.thoughtworks.parking_lot.model.ParkingLot;
+import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ParkingLotController.class)
 public class ParkingLotControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    private ParkingLotRepository parkingLotRepository;
+
+    @Test
+    void should_return_parking_lot_when_create_parking_lot() throws Exception{
+        ParkingLot parkingLot = new ParkingLot("lot1",20,"zha");
+
+        when(parkingLotRepository.save(any())).thenReturn(parkingLot);
+
+        ResultActions resultActions = mockMvc.perform(post("/parkinglots").contentType(MediaType.APPLICATION_JSON_UTF8)
+        .content(new ObjectMapper().writeValueAsString(parkingLot)));
+
+        resultActions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is("lot1")));
+    }
 }
